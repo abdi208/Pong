d57346d6c9e4ee33969a6be918bd9c1fed633b12
Ha-g <ha-g@Ha-gs-MacBook-Pro.local>
2019-09-27 17:02:43 -0700

    trying to detect collision

--- main.js
+++ main.js
@@ -8,50 +8,57 @@ var top2x = 225;
 var cirX = 245;
 var cirY = 245;
 var gameOver = false;
-var i = 0;
+var ballAnimated = setInterval(moveBall, 108);
+
 // var cirY =
-// console.log(ball.offsetTop);
+console.log(ball.offsetLeft);
 // console.log(ball.offsetLeft);
 // console.log(player2.offsetLeft);
 
 
-console.log(player1.offsetTop)
+// console.log(player1.offsetTop)
 document.addEventListener("keydown", function(e) {
     // console.log(e.which);
-    // movePlayerOne(e);
-    // movePlayer2(e);
-    moveBall(e);
+    movePlayerOne(e);
+    movePlayer2(e);
     // console.log(player1.offsetTop)
     // console.log(player2.offsetTop)
 })
 
-// function movePlayerOne(e) {
-//     if (e.which === 38 && topX > 5) {
-//         topX -= 20;
-//        player1.style.top = topX + "px";
-//     }  else if (e.which === 40 && topX < 445) {
-//         topX += 20;
-//        player1.style.top = topX + "px";
-//     }
-// }
-// function movePlayer2(e) {
-//     if (e.which === 37 && top2x > 5) {
-//         top2x -= 20;
-//        player2.style.top = top2x + "px";
-//     }  else if (e.which === 39 && top2x < 445) {
-//         top2x += 20;
-//        player2.style.top = top2x + "px";
-//     }
-// }
+function movePlayerOne(e) {
+    if (e.which === 38 && topX > 5) {
+        topX -= 20;
+        player1.style.top = topX + "px";
+    }  else if (e.which === 40 && topX < 445) {
+        topX += 20;
+        player1.style.top = topX + "px";
+    }
+}
+function movePlayer2(e) {
+    if (e.which === 37 && top2x > 5) {
+        top2x -= 20;
+        player2.style.top = top2x + "px";
+    }  else if (e.which === 39 && top2x < 445) {
+        top2x += 20;
+        player2.style.top = top2x + "px";
+    }
+}
 
 function moveBall(e) {
-    if (e.which === 39){
-        console.log(cirX)
-        console.log(cirY)
+        if (cirY > 5 && cirY < 475){
+        // console.log(cirY)
         cirY = cirY + 20;
-        // ball.style.left = Math.floor((Math.random() * 500))+ "px";
         ball.style.left = cirY + "px";
-        ball.style.top = cirY + "px";
-        
+        // ball.style.top = cirY + "px";
+        console.log(cirY)
     }
+    player2Hit(e);
+    
+}
+
+
+function player2Hit(e) {
+   if(moveBall){
+       console.log('hell yah')
+   }
 }
