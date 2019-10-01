9313b65a9ac8fb94d4a0efe4f553dbb9a00e79ad
Ha-g <ha-g@Ha-gs-MacBook-Pro.local>
2019-10-01 09:19:51 -0700

    update score for player upon win

--- main.js
+++ main.js
@@ -3,11 +3,14 @@
 var player1 = document.getElementById("playerSq");
 var player2 = document.getElementById("playerSq2");
 var container = document.getElementById('container');
+var player1Score = document.getElementById('player1Score');
+var player2Score = document.getElementById('player2Score');
+var score = 0;
 var player1Pos = 225;
 var player2Pos = 225;
 
 //---set interval for the moveBAll callback----
-var ballAnimated = setInterval(moveBall, 100);
+// var ballAnimated = setInterval(moveBall, 100);
 document.addEventListener("keydown", function(e) {
     movePlayerOne(e);
     movePlayer2(e);
@@ -19,7 +22,7 @@ function movePlayerOne(e) {
     if (e.which === 38 && player1Pos > 5) {
         player1Pos -= 20;
         player1.style.top = player1Pos + "px";
-    }  else if (e.which === 40 && player1Pos < 445) {
+    }else if (e.which === 40 && player1Pos < 445) {
         player1Pos += 20;
         player1.style.top = player1Pos + "px";
     }
@@ -28,7 +31,7 @@ function movePlayer2(e) {
     if (e.which === 37 && player2Pos > 5) {
         player2Pos -= 20;
         player2.style.top = player2Pos + "px";
-    }  else if (e.which === 39 && player2Pos < 445) {
+    }else if (e.which === 39 && player2Pos < 445) {
         player2Pos += 20;
         player2.style.top = player2Pos + "px";
     }
@@ -42,21 +45,28 @@ var ball1 =  {
     directionX: 1,
     directionY: -1
 }
+var ball =  ball1;
 // ----make sure the ball moves and detect collision
 function moveBall() {
-        var ball =  ball1;
+        
         ball.x += ball.speed * ball.directionX;
         ball.y += ball.speed * ball.directionY;
         document.getElementById("circle").style.left = ball.x + 'px';
         document.getElementById("circle").style.top = ball.y + 'px';
 
-// --if the top or the bottom of the container is hit send the pong Opposite direction
+        containerBorderHit();
+        detectPaddleHit();
+ 
+}
+function containerBorderHit() {
+    
+        // --if the top or the bottom of the container is hit send the pong Opposite direction
         if (ball.y + ball.speed  * ball.directionY === 500) {
-                ball.directionY = -1;
-			}
+            ball.directionY = -1;
+        }
         if (ball.y + ball.speed  * ball.directionY < 0) {
-                ball.directionY = 1;
-            }
+            ball.directionY = 1;
+        }
 // --- if the pong passes the left side of the container score point for appropriate player serve it back to left side
         if(ball.x + ball.speed * ball.directionX <= 0) {
             ball.x = 245;
@@ -64,7 +74,8 @@ function moveBall() {
             document.getElementById("circle").style.left = ball.x + 'px';
             document.getElementById("circle").style.top = ball.y + 'px';
             ball.directionX = -1
-            
+            score++
+            player2Score.textContent = score
         }
 // -- if pong passes right side of container reset at middle and serve the pong back to losing player
         if(ball.x + ball.speed * ball.directionX > 500) {
@@ -73,30 +84,33 @@ function moveBall() {
             document.getElementById("circle").style.left = ball.x;
             document.getElementById("circle").style.top = ball.y ;
             ball.directionX = 1
+            score++
+            player1Score.textContent = score
+        }
+}
+
+function detectPaddleHit() {
+    var player2Paddle = player2.offsetLeft + player2.offsetWidth
+    var player2Top = player2.offsetTop
+    var player2Height =  player2.offsetHeight + player2Top
+
+    var player1Paddle = player1.offsetLeft + player1.offsetWidth
+    var player1Top = player1.offsetTop
+    var player1Height =  player1.offsetHeight + player1Top
+
+    //--- check to see if pong hit right paddle--/
+    if(ball.x + ball.speed * ball.directionX >= player2Paddle){
+        if(ball.y + ball.speed * ball.directionY <= player2Height && ball.y + ball.speed * ball.directionY >= player2Top){
+            ball.directionX = -1
             
         }
-        var player2Paddle = player2.offsetLeft
-        var player2Top = player2.offsetTop
-        var player2Height =  player2.offsetHeight + player2Top
-//--- check to see if pong hit right paddle--/
-        if(ball.x + ball.speed * ball.directionX >= player2Paddle){
-            if(ball.y + ball.speed * ball.directionY <= player2Height && ball.y + ball.speed * ball.directionY >= player2Top){
-                console.log("big dawg")
-                ball.directionX = -1
-                
-            }
-        }
-
-        var player1Paddle = player1.offsetLeft + player1.offsetWidth
-        var player1Top = player1.offsetTop
-        var player1Height =  player1.offsetHeight + player1Top
+    }
+    
 //--- check to see if pong hit left paddle--/
-        if(ball.x + ball.speed * ball.directionX < player1Paddle){
-            if(ball.y + ball.speed * ball.directionY <= player1Height && ball.y + ball.speed * ball.directionY >= player1Top){
-                console.log("big dawg")
-                ball.directionX = 1
-                
-            }
-        }   
+    if(ball.x + ball.speed * ball.directionX < player1Paddle){
+        if(ball.y + ball.speed * ball.directionY <= player1Height && ball.y + ball.speed * ball.directionY >= player1Top){
+            ball.directionX = 1
+            
+        }
+    }  
 }
- 
